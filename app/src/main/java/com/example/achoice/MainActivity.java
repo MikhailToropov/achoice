3766b410fa06f34abb095efb10274b2a6dfe6d7d
Mikhail Toropov <mihail.toropov@gmail.com>
2019-12-25 20:56:08 +0100

    First working commit

--- app/src/main/java/com/example/achoice/MainActivity.java
+++ app/src/main/java/com/example/achoice/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.achoice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txt;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = findViewById(R.id.textView);
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random r = new Random();
+                int rnd = r.nextInt(4);
+                if (rnd%2==0){
+                    txt.setText("Yes");
+                }
+                else{
+                    txt.setText("No");
+                }
+            }
+        });
     }
 }
